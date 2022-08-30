6df3fc8b0cea4fb359c805430303e3bee6b3cbea
Your Name <you@example.com>
2022-08-30 18:50:22 -0400

    Terraform Cloud Version Control

--- Module.tf
+++ Module.tf
@@ -66,6 +66,6 @@ module "loadbalancing" {
 }
 
 
-  region = "eu-west-2"
-  access_key = "AKIAR6GVIZU3FVO3GC62"
-  secret_key = "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"
+  # region = "eu-west-2"
+  # access_key = "AKIAR6GVIZU3FVO3GC62"
+  # secret_key = "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"
