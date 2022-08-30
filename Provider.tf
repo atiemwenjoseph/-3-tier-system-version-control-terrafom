59f8d12f88820012da18ee177cb369d5851afe72
Your Name <you@example.com>
2022-08-30 19:43:23 -0400

    Terraform Cloud Version Control

--- Provider.tf
+++ Provider.tf
@@ -7,13 +7,5 @@ terraform {
   }
 }
 
-# provider "aws" {
-#   region = "eu-west-2"
-#   endpoints {
-# sts = "https://sts.eu-west-1.amazonaws.com"
-# # }
-# assume_role {
-# role_arn = "role_name value"
-# session_name = "role_session_name"
-# }
-# }
+provider "aws" {
+}
