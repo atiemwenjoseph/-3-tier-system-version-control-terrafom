9ca603f0861f098c08ca30cd933d07e99569d95f
Your Name <you@example.com>
2022-08-30 18:41:27 -0400

    Terraform Cloud Version Control

--- Module.tf
+++ Module.tf
@@ -1,5 +1,7 @@
 ############## Module File #############
-
+  region = "eu-west-2"
+  access_key = "AKIAR6GVIZU3FVO3GC62"
+  secret_key = "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"
 
 locals {
   cwd           = reverse(split("/", path.cwd))
