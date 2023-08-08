a582ea8d77d7151cb6358880abc41d694bc00a6b
atiemwenjoseph <101192905+atiemwenjoseph@users.noreply.github.com>
2023-08-08 15:18:48 +0100

    Update Module.tf

--- Module.tf
+++ Module.tf
@@ -64,8 +64,3 @@ module "loadbalancing" {
   listener_protocol       = "HTTP"
   azs                     = 2
 }
-
-
-  # region = "eu-west-2"
-  # access_key = "AKIAR6GVIZU3FVO3GC62"
-  # secret_key = "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"
